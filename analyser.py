3c0bde7f01913f58f9f8aaa7aa6b1dc43f79c456
Jeffrey Zhou <76892085+jeffreyzhr@users.noreply.github.com>
2022-08-22 22:33:23 -0700

    data analysis 1

--- analyser.py
+++ analyser.py
@@ -1,4 +1,3 @@
-import requests
 import pandas as pd
 import numpy as np
 import yfinance as yf
@@ -6,7 +5,7 @@ import matplotlib.pyplot as plt
 from datetime import date, timedelta
 
 
-def execute(ticker, current_year, write = True):
+def execute(ticker, current_year, dictionary, write = True):
     current = yf.Ticker(ticker)
     #quarterly data
     cashflow = current.quarterly_cashflow
@@ -18,12 +17,16 @@ def execute(ticker, current_year, write = True):
     years_index.append(current_year)
     annual_earnings = current.earnings["Earnings"].to_dict() # earnings dict with years as keys
     shares = current.shares['BasicShares'].to_dict() # shares dict with years as keys
-
     fivey_hist = current.history(period="5y")
+    
+    dictionary[f'{ticker} Annual Earnings'] = annual_earnings
+    dictionary[f'{ticker} Annual Outstanding Shares'] = shares
 
     #method to get quarterly filing dates. Returns list of dates
-    file_dates = [(date.today()- timedelta(days = 1)).strftime('%Y-%m-%d')]
-    for day in balance_sheet.columns.to_list():
+    file_dates = []
+    quarter_dates = balance_sheet.columns.to_list()
+    quarter_dates.insert(0, date.today() - timedelta(days = 1))
+    for day in quarter_dates:
         while day.strftime('%Y-%m-%d') not in fivey_hist["Close"].keys():
             day = day - pd.Timedelta(days=1)
         file_dates.insert(0, day.strftime('%Y-%m-%d'))
@@ -44,6 +47,7 @@ def execute(ticker, current_year, write = True):
     for year in years_index:
         eps = annual_earnings.get(year)/shares.get(year)
         annual_EPS[year] = eps
+    dictionary[f'{ticker} Annual EPS'] = annual_EPS
 
     # Splitting 5yr hist into respective years
     years_index_timestamp = [pd.Timestamp(year = y, month = 1, day = 1) for y in years_index]
@@ -56,6 +60,7 @@ def execute(ticker, current_year, write = True):
         close = current.history(start = start, end=cutoff)
         yearly_hist[years_index[i]] = close
     yearly_hist[years_index[-1]] = current.history(start = years_index_timestamp[-1], end=None)
+    dictionary[f'{ticker} Yearly Hist values'] = yearly_hist #reminder that yearly hist is a dict with years as keys has has cols (open, close, volume etc)
 
     # Getting daily PE ratios for each year based on annual_EPS and yearly_hist
     yearly_PE = {}
@@ -63,5 +68,4 @@ def execute(ticker, current_year, write = True):
         close = yearly_hist[year]['Close'].to_list()
         PE_ratio = [price/annual_EPS[year] for price in close]
         yearly_PE[year] = PE_ratio
-    
-    return
+    dictionary[f'{ticker} Annual PE ratio'] = yearly_PE #daily PE ratio for each year
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import pandas as pd
-import numpy as np
-import yfinance as yf
 import matplotlib.pyplot as plt
 from datetime import date
-import analyser
+import analyser 
+import pickle
+import pandas as pd
+
 
 '''
 this is the main python file that will run the analyser on various companies and produce a markdown file
 '''
-print(1)
-ticker = 'AAPL'
-current_year = date.today().year
 
-analyser.execute('AAPL', current_year, write=False)
+current_year = date.today().year
+sp500_tickers = pd.read_csv('Stocks in the SP 500 Index.csv')['Symbol'].to_list()
+
+def save(ticker):
+    with open(f'data/{ticker}.pkl', 'wb') as f:
+        pickle.dump(to_save_dict, f)
+
+count = 1
+for ticker in sp500_tickers:
+    to_save_dict = {}
+    analyser.execute('AAPL', current_year, dictionary=to_save_dict, write=False)
+    save(ticker)
+    print(f'{ticker}: DONE ({count}/500)')
+    count += 1
